642f6331c5b0cca1065e2ed8874a5eea9afbb04b
“Valentin <apovalov@gmail.com>
2023-12-11 16:33:24 +0700

    Updated

--- README.md
+++ README.md
@@ -1,27 +1,94 @@
-# Репозиторий для задачи Suggest Service
+# Suggest Service / Prefix tree
 
-## Запуск сервиса
+Let's imagine that every time a user starts typing a query, our system starts "searching" through all the records in your database to find relevant variants (even using binary search!). It's as if every time we search for one particular book in a huge library, we would start browsing each shelf first, hoping to find the right volume. Not only would this take a lot of time, but it could also cause excessive load on the server.
 
-Команда запуска:
+Now imagine your database consists of tens of millions of queries. A direct binary search over that much data may take a few seconds, which seems like an instant for the scale of such a database, but in reality it is infinitely long for a user who expects a responsive interface, seamless UX and hence instantaneous system response. Moreover, this approach would be resource-intensive, increasing the cost of maintaining servers.
 
-`bash run.sh`
+That's why a simple search (even if the queries are sorted, which would allow binary search) is still not an effective solution. We need a tool that allows us to instantly find relevant queries without having to look through each one. What if we pre-prepare a data structure that takes into account the user's typing structure and logic when formulating a query?
 
-## Тесты
+Well, welcome to the world of prefix trees!
 
-Команда запуска:
+## Prefix trees
 
-`pytest .`
+A prefix tree or simply Trie is a tree-like data structure used to store a dynamic set of strings (we will also call them queries). Each node represents a single character, and strings are formed by tracing a path from the root of the tree to a particular node. This allows us to quickly find all matching queries starting with a given prefix, without having to "search" the entire database.
 
-Запуск с подробным выводом (ошибки и prints):
+A prefix is a part of a word that starts with the first letter and ends with any of the following letters. For example, the prefixes of the word "computer" are "k", "ko", "com", "comp" and so on.
 
-`pytest -svv .`
+The suffix of a word is the part of the word that begins with any of the letters of the word and ends with the last letter.
 
-Запуск одного теста (если нужно тестировать конкретную функцию):
+In the Trie context, knowing the prefix of a word, it is possible to find all its suffixes stored in the tree.
 
-`pytest -svv tests/test_app.py::test_suggestions_recall`
+This is accomplished by "descending" from the last character of the prefix down the tree. Thus, the prefix "ap" can lead us to the suffixes "i" or "ple" if both of these words are stored in the tree.
+
+![Alt text](image.png)
+
+
+The main advantages of prefix trees:
+
+Speed - they allow us to quickly retrieve strings with a common prefix. For example, if a user starts typing "com", our tree can quickly show sentences such as "computer", "compact" and "company".
+Memory efficiency - the tree may seem wasteful in terms of memory, but its structure ensures that common prefixes are only stored once. This results in significant memory savings, especially for large datasets.
+Dynamism - trie can be updated. This means that as new search queries become popularized, we can seamlessly integrate them into our existing tree.
+Autocomplete - this is the backbone of our hinting service. As users enter their queries, trees can predict and suggest the most likely completions.
+
+
+## Relevance evaluation
+If you represent a user query as a prefix, it's not hard to get the right suffixes.
+
+But how to sort them and select the top-k queries for a user?
+
+To solve this problem, we use a ranking system based on the popularity of queries. Each query in our tree is assigned a certain weight (a positive value). The more often users search for a certain query, the higher its weight. It is by this weight that we rank the hints (you can learn more about the ranking metrics in the NDCG task), offering first the ones most frequently searched by other users.
+
+In addition, thanks to the flexibility of prefix trees, we can integrate machine learning-based weights, further improving the relevance and accuracy of our hints.
+
+
+##Reverse tree
+At first glance, prefix trees look perfect for providing quick hints based on the initial characters of the user's input. However, what if the user searches using part of a word or phrase that is in the middle or even at the end?
+
+A Reversed Trie is a prefix tree in which strings are added in reverse order. Thus, it allows you to efficiently find prefixes for a given suffix or, in other words, it improves substring search.
+
+## Autocomplete Suggestions Formatting
+
+The formatting of autocomplete suggestions is a crucial aspect of the user experience and typically begins with a Trie or a Reversed Trie to generate the basic suggestions. However, to enhance the quality and relevance of these suggestions, additional techniques are employed.
+
+### Suggestion Methods
+
+Here are some of the methods from our project's code that shape the user's search query:
+
+1. `suggest_query`: The primary method that generates suggestions based on the user's input.
+
+2. `suggest_removed_char`: A method that removes the last character from the query and provides suggestions for the modified query.
+
+3. `suggest_last_words`: This method offers suggestions based on the prefixes of the last words of the query.
+
+4. `suggest_each_word`: In this method, suggestions are regenerated for each word in the query independently.
+
+After applying these methods, a comprehensive list of potential suggestions is formed. All collected suggestions are sorted according to their relevance.
+
+In the end, the user is presented with the most suitable suggestion.
+
+
+## Start service
+
+Run command:
+
+`bash run.sh`.
+
+## Tests
+
+Run command:
+
+`pytest .`.
+
+Run with detailed output (errors and prints):
+
+`pytest -svv .`.
+
+Run a single test (if you want to test a specific function):
+
+`pytest -svv tests/test_app.py::test_suggestions_recall`.
 
 ## Pylint
 
-Команда запуска:
+Run command:
 
-`pylint app.py`
+`pylint app.py`.
---
+++ sandbox.py
@@ -0,0 +1,548 @@
+import re
+from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import Dict, List, Tuple
+
+import tqdm
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from src.utils import download_yandex_disk
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+QUERIES_URL = "https://disk.yandex.ru/d/yQt-jfBSzTs1eA"
+QUERIES_PATH = "data/queries.txt"
+
+
+@dataclass
+class Node:
+    """
+    Represents a Trie node.
+
+    Attributes
+    ----------
+    children : Dict[str, "Node"]
+        Dictionary of child nodes.
+    is_end : bool
+        Indicates if this node is an ending character of a stored string.
+    value : float
+        Value associated with the stored string.
+    """
+
+    children: Dict[str, "Node"] = field(default_factory=dict)
+    is_end: bool = False
+    value: float = 0
+
+
+@dataclass
+class Trie:
+    """
+    A Trie data structure for efficient string manipulation and retrieval.
+
+    Attributes
+    ----------
+    root : Node
+        The root node of the Trie.
+    """
+
+    root: Node = field(default_factory=Node)
+
+    def add_query(self, query: str, value: float) -> None:
+        node = self.root
+        for char in query:
+            if char not in node.children:
+                print(f"Создаем узел '{char}'")  # Отладочный вывод
+                node.children[char] = Node()
+            else:
+                print(f"Переходим к существующему узлу '{char}'")  # Отладочный вывод
+            node = node.children[char]
+        node.is_end = True
+        node.value = value
+        print(f"Добавлена строка '{query}', значение: {value}")  # Отладочный вывод
+
+    def remove_query(self, query: str) -> None:
+        def remove_recursively(node, query, depth):
+            if depth == len(query):
+                if not node.is_end:
+                    raise Exception(f"Query {query} not found!")
+                node.is_end = False
+                return len(node.children) == 0
+
+            char = query[depth]
+            if char not in node.children:
+                raise Exception(f"Query {query} not found!")
+
+            should_delete_child_node = remove_recursively(node.children[char], query, depth + 1)
+
+            if should_delete_child_node:
+                del node.children[char]
+                return len(node.children) == 0
+
+            return False
+
+        remove_recursively(self.root, query, 0)
+
+    def clear(self) -> None:
+        self.root = Node()
+
+    def suffixes(self, prefix: str) -> List[Tuple[float, str]]:
+        def find_node(node, prefix):
+            for char in prefix:
+                if char in node.children:
+                    print(f"Переходим к узлу '{char}'")  # Отладочный вывод
+                    node = node.children[char]
+                else:
+                    print(f"Узел '{char}' не найден")  # Отладочный вывод
+                    return None
+            return node
+
+        def collect_suffixes(node, prefix, results):
+            if node.is_end:
+                print(f"Найден суффикс: {prefix}, значение: {node.value}")  # Отладочный вывод
+                results.append((node.value, prefix))
+            for char, child_node in node.children.items():
+                collect_suffixes(child_node, prefix + char, results)
+
+        print(f"Поиск суффиксов для префикса: {prefix}")  # Отладочный вывод
+        results = []
+        node = find_node(self.root, prefix)
+        if node:
+            collect_suffixes(node, prefix, results)
+        return results
+
+    def count_queries(self) -> int:
+        def count_nodes(node):
+            count = 1 if node.is_end else 0
+            for child in node.children.values():
+                count += count_nodes(child)
+            return count
+
+        return count_nodes(self.root)
+
+    def print_trie(self, node=None, prefix=""):
+        if node is None:
+            node = self.root
+        if node.is_end:
+            print(f"String: {prefix}, Value: {node.value}")
+        for char, next_node in node.children.items():
+            self.print_trie(next_node, prefix + char)
+
+
+@dataclass
+class ReversedTrie(Trie):
+    """
+    A Reversed Trie data structure derived from the Trie,
+    which efficiently manipulates and retrieves reversed strings.
+
+    Give a possibility to find prefixes of the given suffix.
+
+    Notes
+    -----
+    The ReversedTrie is derived from the Trie class, which means that
+    all the methods of the Trie class are available for the ReversedTrie class.
+
+    Use super() to call the methods of the Trie class.
+    """
+
+    def add_query(self, query: str, value: float) -> None:
+        """
+        Adds a single reversed query string to the Trie.
+
+        Parameters
+        ----------
+        query : str
+            The string whose reverse will be added to the Trie.
+        value : float
+            The value associated with the query.
+
+        Examples
+        --------
+        Given queries: "apple", "triple"
+
+        # >>> rtrie = ReversedTrie()
+        # >>> rtrie.add_query("apple", 1.0)
+        """
+        reversed_query = query[::-1]
+        print(f"Добавляем строку в обратном порядке: {reversed_query}")  # Отладочный вывод
+        super().add_query(reversed_query, value)
+
+    def prefixes(self, suffix: str) -> List[Tuple[float, str]]:
+        """
+        Returns all prefixes of the given suffix.
+
+        Notes
+        -----
+        Here by prefix we mean string prefix + suffix.
+
+        Parameters
+        ----------
+        suffix : str
+            The suffix string.
+
+        Returns
+        -------
+        List[Tuple[float, str]]
+            List of (value, prefix) pairs.
+
+        Examples
+        --------
+        """
+        reversed_suffix = suffix[::-1]
+        print(f"Поиск префиксов для суффикса: {suffix}, обратный суффикс: {reversed_suffix}")  # Отладочный вывод
+        suffixes = super().suffixes(reversed_suffix)
+
+        return [(value, prefix[::-1]) for value, prefix in suffixes]
+
+
+# # Дополнительная отладка метода prefixes в ReversedTrie
+# rtrie = ReversedTrie()
+# rtrie.add_query("test", 1.0)  # Добавляется как 'tset'
+#
+# print("Отладка метода prefixes в ReversedTrie для суффикса 'ts':")
+# result = rtrie.prefixes("ts")  # Ищем префиксы для 'ts', что в обратном порядке 'st'
+# print(f"Результат поиска префиксов: {result}")
+
+
+@dataclass
+class Suggester:
+    """
+    A class to provide string suggestions based on input
+    using both standard and reverse Trie data structures.
+
+    Notes
+    -----
+    Make sure that suggest_ methods return unique suggests Tuple.
+
+    Attributes
+    ----------
+    trie : Trie
+        A standard trie data structure for forward string manipulations.
+    reversed_trie : ReversedTrie
+        A reverse trie data structure for backward string manipulations.
+    """
+
+    trie: Trie = field(default_factory=Trie)
+    reversed_trie: ReversedTrie = field(default_factory=ReversedTrie)
+
+    def fit(self, queries: Dict[str, float]) -> None:
+        """
+        Fits the suggester with a dictionary of queries and associated values.
+
+        Parameters
+        ----------
+        queries : Dict[str, float]
+            A dictionary of query strings and their associated values.
+        """
+        for query, value in queries.items():
+            self.trie.add_query(query, value)
+
+            # Добавляем все префиксы в обратном порядке в ReversedTrie
+            for i in range(1, len(query) + 1):
+                reversed_prefix = query[:i] #[::-1]  # Берем префикс и разворачиваем его
+                self.reversed_trie.add_query(reversed_prefix, value)
+
+    def count_queries(self) -> int:
+        """
+        Returns the total number of queries in both tries.
+
+        Returns
+        -------
+        int
+            Total number of queries.
+        """
+        return self.trie.count_queries() + self.reversed_trie.count_queries()
+
+    def suggest_query(self, query: str) -> List[Tuple[float, str]]:
+        """
+        Provides suggestions based on a given query string.
+
+        Also provides suggestions based on ReversedTrie prefixes.
+
+        Hint
+        ----
+        Use the ReversedTrie prefixes method.
+
+        Parameters
+        ----------
+        query : str
+            The input string.
+
+        Returns
+        -------
+        List[Tuple[float, str]]
+            A list of suggested queries with their associated values.
+
+        Examples
+        --------
+        # >>> suggester = Suggester()
+        # >>> suggester.fit({"apple": 1.0, "triple": 2.0})
+        # >>> suggester.suggest_query("pl")
+        [(1.0, 'apple'), (2.0, 'triple')]
+        """
+        # suggestions = set(self.trie.suffixes(query))
+        # suggestions.update((val, prefix) for val, prefix in self.reversed_trie.prefixes(query))
+        # return sorted(suggestions, key=lambda x: -x[0])
+
+        trie_suggestions = set(self.trie.suffixes(query))
+        reversed_trie_prefixes = self.reversed_trie.prefixes(query)
+
+        # Используем префиксы из ReversedTrie для поиска в Trie
+        for _, prefix in reversed_trie_prefixes:
+            trie_suggestions.update(self.trie.suffixes(prefix))
+
+        # Комбинирование и сортировка результатов
+        unique_suggestions = sorted(trie_suggestions, key=lambda x: -x[0])
+        return unique_suggestions
+
+
+    def suggest_removed_char(self, query: str) -> List[Tuple[float, str]]:
+        """
+        Provides suggestions based on the query after removing the last character.
+
+        Return [] if the query length is less than 2.
+
+        Hint
+        ----
+        Reuse self.suggest_query instead of justs self.trie.suffixes.
+
+        Parameters
+        ----------
+        query : str
+            The input string.
+
+        Returns
+        -------
+        List[Tuple[float, str]]
+            A list of suggested queries with their associated values.
+        """
+        if len(query) < 2:
+            return []
+        return self.suggest_query(query[:-1])
+
+    def suggest_last_words(self, query: str) -> List[Tuple[float, str]]:
+        """
+        Provides suggestions based on query words prefix.
+
+        Parameters
+        ----------
+        query : str
+            The input string, typically containing multiple words.
+
+        Returns
+        -------
+        List[Tuple[float, str]]
+            A list of suggested queries with their associated values.
+
+        Examples
+        --------
+        Psuedo code:
+        query = "apple iphone banana"
+        suggestions =
+            + suggest_query("apple iphone")
+            + suggest_query("apple")
+        """
+        words = query.split()
+        suggestions = set()
+        for i in range(len(words)):
+            sub_query = " ".join(words[i:])
+            suggestions.update(self.suggest_query(sub_query))
+        return sorted(suggestions, key=lambda x: -x[0])
+
+
+    def suggest_each_word(self, query: str) -> List[Tuple[float, str]]:
+        """
+        Provides suggestions based on each word in the query.
+
+        Parameters
+        ----------
+        query : str
+            The input string, typically containing multiple words.
+
+        Returns
+        -------
+        List[Tuple[float, str]]
+            A list of suggested queries with their associated values.
+
+        Examples
+        --------
+        Psuedo code:
+        query = "apple iphone banana"
+        suggestions =
+            + suggest_query("apple")
+            + suggest_query("iphone")
+            + suggest_query("banana")
+        """
+        words = query.split()
+        suggestions = set()
+        for word in words:
+            suggestions.update(self.suggest_query(word))
+        return sorted(suggestions, key=lambda x: -x[0])
+
+
+
+# def sample_suggester():
+#     suggester = Suggester()
+#     suggester.fit(
+#         {
+#             "apple pie": 1.0,
+#             "pie apple": 20.0,
+#             "triple threat": 2.0,
+#             "banana boat": 3.0,
+#             "grape juice": 4.0,
+#             "apple bubble": 1.0,
+#             "blueberry muffin": 5.0,
+#             "i love apple": 2.0,
+#         }
+#     )
+#     return suggester
+#
+# suggester = sample_suggester()
+#
+# # print('print_trie')
+# # print(suggester.reversed_trie.print_trie() )
+#
+# print('suggest_query')
+# results = suggester.suggest_query("ple")
+# print(results)
+
+
+suggester = Suggester()
+
+
+def preprocess_query(query: str) -> str:
+    """
+    Preprocess the given query string.
+
+    Based on the queries.txt file understand
+    how to preprocess the query string.
+
+    Parameters
+    ----------
+    query : str
+        The raw input string that needs to be preprocessed.
+
+    Returns
+    -------
+    str
+        The preprocessed query string.
+
+    Examples
+    --------
+    # >>> preprocess_query("  HelLo,  ;  World!  ")
+    'hello world'
+    """
+    query = re.sub(r'[^\w\s]', '', query)  # Удаляем пунктуацию
+    query = re.sub(r'\s+', ' ', query)    # Удаляем лишние пробелы
+    return query.strip().lower()
+
+
+def count_queries(file) -> Dict[str, float]:
+    """
+    Counts the number of times each query appears in the file.
+
+    The value of each query is defined by number of times
+    the preprocess query (preprocess_query(q)) appears in the file.
+
+    Parameters
+    ----------
+    file : file
+
+    Returns
+    -------
+    Dict[str, float]
+        A dictionary of query strings and their associated values.
+
+    Examples
+    --------
+    file:
+        appLe 123
+        apple  123;
+        bana;na
+        banana
+        bananA
+    # >>> with open("queries.txt", "r") as file:
+    # >>>     count_queries(file)
+    {'apple 123': 2.0, 'banana': 3.0}
+    """
+    queries: Dict[str, float] = defaultdict(lambda: 0)
+    rows = file.readlines()
+    for line in tqdm.tqdm(rows):
+        preprocessed_query = preprocess_query(line)
+        queries[preprocessed_query] += 1.0
+    return queries
+
+
+@app.on_event("startup")
+async def startup_event() -> None:
+    """
+    Handles the startup event of the FastAPI application.
+    Downloads the queries file and fit the suggester with the queries.
+
+    The value of each query is defined by number of times
+    the preprocess query (preprocess_query(q)) appears in the file.
+    """
+    # download the queries file
+    print("Downloading queries file...")
+    download_yandex_disk(QUERIES_URL, QUERIES_PATH)
+    with open(QUERIES_PATH, "r") as file:
+        queries = count_queries(file)
+    suggester.fit(queries)
+    print(f"Suggester fitted with {suggester.count_queries()} queries!")
+
+
+@app.get("/suggest/")
+def suggest(query: str, k: int = 10):
+    """
+    Provide search query suggestions based on the input query.
+
+    Suggestions are sorted by their associated values in descending order.
+
+    Parameters
+    ----------
+    query : str
+        The input search query for which suggestions are needed.
+    k : int, optional
+        The number of suggestions required, by default 10.
+
+    Returns
+    -------
+    Dict[str, List[str]]
+        A dictionary containing the preprocessed query and the list of suggestions.
+
+    Examples
+    --------
+    # >>> suggestions("", k=10)
+    {"query": "", "suggestions": []}
+    # >>> suggestions("HeL", k=3)
+    {"query": "hel", "suggestions": ["hello world", "help", "helmet"]}
+    """
+
+    query = preprocess_query(query)
+    suggestions = suggester.suggest_query(query)
+    suggestions += suggester.suggest_removed_char(query)
+    if len(set(suggestions)) < k:
+        suggestions += suggester.suggest_last_words(query)
+        suggestions += suggester.suggest_each_word(query)
+    # Уникальные предложения, отсортированные по значению и взятие первых k элементов
+    unique_suggestions = sorted(set(suggestions), key=lambda x: -x[0])[:k]
+    return {"query": query, "suggestions": [s[1] for s in unique_suggestions]}
+
+
+
+@app.get("/")
+def root(request: Request):
+    """
+    Returns html page of the application.
+
+    Parameters
+    ----------
+    request : Request
+        The request object.
+    """
+    return templates.TemplateResponse("index.html", {"request": request})
